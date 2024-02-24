07f127c4c23fa8ea69a3f6a2f8062023427a3d05
EverAlon <everalon17@gmail.com>
2024-02-23 18:09:12 -0600

    producto

--- src/config/rutas.js
+++ src/config/rutas.js
@@ -1,5 +1,5 @@
 export const URL_API = "https://apfirebase.onrender.com/api/"
-export const URL_IMAGES = "https://apfirebase.onrender.com/images/"
+export const URL_IMAGES = "https://apfirebase.onrender.com/Usuarios/images/"
 
 export const URL_API_PRODUC = "https://apfirebase.onrender.com/api/"
-export const URL_IMAGES_PRODUC = "http://localhost:3000/Productos/images/"
+export const URL_IMAGES_PRODUC = "https://apfirebase.onrender.com/Productos/images/"
